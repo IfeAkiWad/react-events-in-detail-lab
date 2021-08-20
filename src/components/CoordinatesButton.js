d30b04e20ad6d44c914a91c92a682365c1616230
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-08-19 19:12:59 -0500

    commit tree 59215ff9a26fdbde98c6b8fdf7cc92fb05fc060b

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -3,6 +3,18 @@
 import React, { Component } from 'react' 
 
 export default class CoordinatesButton extends Component {
-    this.props.onReceiveCoordinates
+    // this.props.onReceiveCoordinates
+
+    clickEvent = (event) => {
+        this.props.onReceiveCoordinates([event.clientX, event.clientY])
+    }
+
+    render() {
+        return (
+            <button onClick={this.clickEvent}>
+
+            </button>
+        )
+    }
 
 }
